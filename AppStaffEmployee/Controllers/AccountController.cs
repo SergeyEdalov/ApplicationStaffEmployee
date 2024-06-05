d5c1c3e1a279f47ac38376659d55b0869f9446c3
SergeyEdalov <edalov94@mail.ru>
2024-06-05 13:07:27 +0300

    Сделан вход в систему пользователя

--- AppStaffEmployee/Controllers/AccountController.cs
+++ AppStaffEmployee/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ public class AccountController : Controller
     private readonly ILogger<AccountController> _logger;
 
     public AccountController(
-        UserManager<User> userManager, 
+        UserManager<User> userManager,
         SignInManager<User> signInManager, ILogger<AccountController> logger)
     {
         _userManager = userManager;
@@ -20,6 +20,7 @@ public class AccountController : Controller
         _logger = logger;
     }
 
+    #region Регистрация нового пользователя
     public IActionResult Register()
     {
         return View(new RegisterUserViewModel());
@@ -45,23 +46,53 @@ public class AccountController : Controller
 
         foreach (var error in result.Errors)
         {
-            ModelState.AddModelError("", error.Description);   
+            ModelState.AddModelError("", error.Description);
         }
         return View(model);
     }
+    #endregion
 
-    public async Task<IActionResult> Login()
+
+    #region Вход в систему
+    public IActionResult Login(string? ReturnUrl = null)
     {
-        return View();
-    }   
+        return View(new LoginViewModel
+        {
+            ReturnUrl = ReturnUrl
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (!ModelState.IsValid) return View(model);
+
+        var result = await _signInManager.PasswordSignInAsync(
+            model.UserName, 
+            model.Password,
+            model.RememberMe,
+            true);
+
+        if (result.Succeeded)
+        {
+            _logger.LogInformation("Пользователь {0} успешно вошел в систему", model.UserName);
+            
+            return LocalRedirect(model.ReturnUrl ?? "/");
+        }
+
+        ModelState.AddModelError("", "Неверное имя пользователя или пароль");
+        _logger.LogWarning("Ошибка при входе в систему {0}", model.UserName);
+        
+        return View(model);
+    }
+    #endregion
     public async Task<IActionResult> Logout()
     {
-        return View();
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
     }   
     public async Task<IActionResult> AccessDenied()
     {
         return View();
     }
-
-
 }
