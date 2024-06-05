173b2a45659f220dc6e79f541c217aadb79bf61d
SergeyEdalov <edalov94@mail.ru>
2024-06-05 12:35:00 +0300

    Сделана регистрация пользователя

--- AppStaffEmployee/Controllers/AccountController.cs
+++ AppStaffEmployee/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Identity.DAL.Entities;
+﻿using AppStaffEmployee.ViewModels.Identity;
+using Identity.DAL.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,23 +7,49 @@ namespace AppStaffEmployee.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly UserManager<User> _user;
+    private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
     private readonly ILogger<AccountController> _logger;
 
     public AccountController(
-        UserManager<User> user, 
+        UserManager<User> userManager, 
         SignInManager<User> signInManager, ILogger<AccountController> logger)
     {
-        _user = user;
+        _userManager = userManager;
         _signInManager = signInManager;
         _logger = logger;
     }
 
-    public async Task<IActionResult> Register()
+    public IActionResult Register()
     {
-        return View();
-    }   
+        return View(new RegisterUserViewModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterUserViewModel model)
+    {
+        if (!ModelState.IsValid) return View(model);
+
+        var user = new User { UserName = model.UserName };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            _logger.LogInformation("Пользователь {0} успешно создан", user);
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError("", error.Description);   
+        }
+        return View(model);
+    }
+
     public async Task<IActionResult> Login()
     {
         return View();
