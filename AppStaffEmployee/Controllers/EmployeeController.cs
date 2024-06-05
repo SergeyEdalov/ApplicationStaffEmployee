70eaf9f421db235a20f3ec696ecc79eb0561f1cf
SergeyEdalov <edalov94@mail.ru>
2024-06-05 13:11:35 +0300

    Сделан выход из системыReapply "Сделан выход из системы и отказ в доступе"
    This reverts commit af0e3dba487c00a19d5bd3da12b1ac69f265034c.

--- AppStaffEmployee/Controllers/EmployeeController.cs
+++ AppStaffEmployee/Controllers/EmployeeController.cs
@@ -2,89 +2,90 @@ using AppStaffEmployee.Models.Dto;
 using AppStaffEmployee.Services.Interfaces;
 using AppStaffEmployee.ViewModels;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ApplicationStaffEmployee.Controllers
+namespace ApplicationStaffEmployee.Controllers;
+
+[Authorize]
+public class EmployeeController : Controller
 {
-    public class EmployeeController : Controller
+    private readonly IEmployeeService<EmployeeDto, Guid> _employeeService;
+    private readonly ILogger<EmployeeController> _logger;
+    private readonly IMapper _mapper;
+
+    public EmployeeController(IEmployeeService<EmployeeDto, Guid> employeeService, ILogger<EmployeeController> logger, IMapper mapper)
     {
-        private readonly IEmployeeService<EmployeeDto, Guid> _employeeService;
-        private readonly ILogger<EmployeeController> _logger;
-        private readonly IMapper _mapper;
+        _employeeService = employeeService;
+        _logger = logger;
+        _mapper = mapper;
+    }
 
-        public EmployeeController(IEmployeeService<EmployeeDto, Guid> employeeService, ILogger<EmployeeController> logger, IMapper mapper)
+    public async Task<IActionResult> Index()
+    {
+        var employeesTable = await _employeeService.GetAllEmployeesAsync();
+        var employeeView = employeesTable.Select(_mapper.Map<EmployeeViewModel>);
+        return View(employeeView);
+    }
+
+    public async Task<IActionResult> Details (Guid id)
+    {
+        var employee = await _employeeService.GetEmpoloyeeByIDAsync(id);
+        if (employee is null)  return NotFound();
+
+        var employeeView = _mapper.Map<EmployeeViewModel>(employee);
+        return View(employeeView);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        return View("Edit", new EmployeeViewModel());
+    }
+
+    public async Task<IActionResult> Edit(Guid? id)
+    {
+        if (id == null) return View(new EmployeeViewModel());
+
+        var employee = await _employeeService.GetEmpoloyeeByIDAsync((Guid)id);
+        if (employee is null) return NotFound();
+
+        var employeeView = _mapper.Map<EmployeeViewModel>(employee);
+        return View(employeeView);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(EmployeeViewModel model)
+    {
+        if (!ModelState.IsValid) return View(model); 
+
+        var employeeDto = _mapper.Map<EmployeeDto>(model);
+
+        if(employeeDto.Id is null)
         {
-            _employeeService = employeeService;
-            _logger = logger;
-            _mapper = mapper;
+            var id = await _employeeService.AddEmployeeAsync(employeeDto);
+            return RedirectToAction("Details", new {id});
         }
+        var success = await _employeeService.EditEmployeeAsync(employeeDto);
+        
+        if(!success) return NotFound();
+        return RedirectToAction("Index");
+    }
 
-        public async Task<IActionResult> Index()
-        {
-            var employeesTable = await _employeeService.GetAllEmployeesAsync();
-            var employeeView = employeesTable.Select(_mapper.Map<EmployeeViewModel>);
-            return View(employeeView);
-        }
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var employee = await _employeeService.GetEmpoloyeeByIDAsync(id);
+        if (employee is null) return NotFound();
 
-        public async Task<IActionResult> Details (Guid id)
-        {
-            var employee = await _employeeService.GetEmpoloyeeByIDAsync(id);
-            if (employee is null)  return NotFound();
+        var employeeView = _mapper.Map<EmployeeViewModel>(employee);
+        return View(employeeView);
+    }
 
-            var employeeView = _mapper.Map<EmployeeViewModel>(employee);
-            return View(employeeView);
-        }
+    [HttpPost]
+    public async Task<IActionResult> DeleteConfirmed(Guid id)
+    {
+        var success = await _employeeService.RemoveEmployeeAsync(id);
+        if (!success) return NotFound();
 
-        public async Task<IActionResult> Create()
-        {
-            return View("Edit", new EmployeeViewModel());
-        }
-
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null) return View(new EmployeeViewModel());
-
-            var employee = await _employeeService.GetEmpoloyeeByIDAsync((Guid)id);
-            if (employee is null) return NotFound();
-
-            var employeeView = _mapper.Map<EmployeeViewModel>(employee);
-            return View(employeeView);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(EmployeeViewModel model)
-        {
-            if (!ModelState.IsValid) return View(model); 
-
-            var employeeDto = _mapper.Map<EmployeeDto>(model);
-
-            if(employeeDto.Id is null)
-            {
-                var id = await _employeeService.AddEmployeeAsync(employeeDto);
-                return RedirectToAction("Details", new {id});
-            }
-            var success = await _employeeService.EditEmployeeAsync(employeeDto);
-            
-            if(!success) return NotFound();
-            return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var employee = await _employeeService.GetEmpoloyeeByIDAsync(id);
-            if (employee is null) return NotFound();
-
-            var employeeView = _mapper.Map<EmployeeViewModel>(employee);
-            return View(employeeView);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var success = await _employeeService.RemoveEmployeeAsync(id);
-            if (!success) return NotFound();
-
-            return RedirectToAction("Index");
-        }
+        return RedirectToAction("Index");
     }
 }
